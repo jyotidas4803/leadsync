2c3868cbbe7cf6ea0823db502989f3472d530f7b
Jyotiii Das <jyotidas4803@gmail.com>
2025-01-29 08:23:12 +0000

    untrack companies.csv

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -1,83 +1,87 @@
 import { describe, expect, test } from "vitest";
-import {
-  isEmail,
-  isLinkedinURL,
-  isCompanyName,
-  isEmployeeSize,
-} from "../lib/validate.js";
 
-describe("Testing isEmail", () => {
-  test("testing for a valid email", () => {
-    expect(isEmail("contact@leadsync.com")).toBe(true);
+import { isURL, isEmail, isEmployeeSize, isCompanyName } from "../lib/validate.js";
+
+// MARK: isURL
+describe("Checking isURL", () => {
+  test("Check for proper URL", () => {
+    expect(isURL("https://google.com")).toBe(true);
   });
 
-  test("testing NA", () => {
+  test("Check for valid URL pattern", () => {
+    expect(isURL("www.google.im")).toBe(true);
+  });
+
+  test("Checks for incomplete pattern", () => {
+    expect(isURL("google")).toBe(false);
+  });
+
+  test("Checks for www", () => {
+    expect(isURL("ww.google.com")).toBe(true);
+  });
+
+  test("Check for both http and www part", () => {
+    expect(isURL("http://www.google.com")).toBe(true);
+  });
+});
+
+// MARK: isEmail
+describe("Checking isEmail", () => {
+  test("Check for proper email", () => {
+    expect(isEmail("hey@website.com")).toBe(true);
+  });
+
+  test("Check for NA", () => {
     expect(isEmail("NA")).toBe(true);
   });
 
-  test("testing for an invalid email", () => {
-    expect(isEmail("hey@phone@book.com")).toBe(false);
+  test("Check for wrong email", () => {
+    expect(isEmail("hey@company")).toBe(false);
   });
 });
 
-describe("Testing isLinkedinURL", () => {
-  test("testing for a valid linkedin url", () => {
-    expect(isLinkedinURL("https://in.linkedin.com/company/google")).toBe(true);
+// MARK: isEmployeeSize
+describe("Checking isEmployeeSize", () => {
+  test("Check for a number", () => {
+    expect(isEmployeeSize("40")).toBe(true);
   });
 
-  test("testing for an invalid linkedin url", () => {
-    expect(isLinkedinURL("https://in.linked.com/company/facebook")).toBe(false);
-  });
-
-  test("testing without http part for incorrect spelling", () => {
-    expect(isLinkedinURL("www.linked.com/qualitysoftech")).toBe(false);
-  });
-
-  test("testing for valid linkedin without http", () => {
-    expect(isLinkedinURL("www.linkedin.com")).toBe(true);
-  });
-});
-
-describe("Testing isCompanyName", () => {
-  test("testing for a alphabetical name", () => {
-    expect(isCompanyName("Phleebs")).toBe(true);
-  });
-
-  test("testing for an alphanumeric one", () => {
-    expect(isCompanyName("e24")).toBe(true);
-  });
-
-  test("testing for URL case", () => {
-    expect(isCompanyName("e24.ai")).toBe(false);
-  });
-
-  test("testing for potential employee-size entry", () => {
-    expect(isCompanyName("10-500")).toBe(false);
-  });
-});
-
-describe("Testing isEmployeeSize", () => {
-  test("testing for a alphabetical name", () => {
-    expect(isEmployeeSize("10")).toBe(true);
-  });
-
-  test("testing for an alphanumeric one", () => {
+  test("Check for a range", () => {
     expect(isEmployeeSize("10-500")).toBe(true);
   });
 
-  test("testing for URL case", () => {
-    expect(isEmployeeSize("100+")).toBe(true);
+  test("Check for k & +", () => {
+    expect(isEmployeeSize("10k+")).toBe(true);
   });
 
-  test("testing for potential employee-size entry", () => {
-    expect(isEmployeeSize("100k")).toBe(true);
+  test("Check for invalid suffix", () => {
+    expect(isEmployeeSize("10lakh")).toBe(false);
   });
 
-  test("testing for potential employee-size entry", () => {
-    expect(isEmployeeSize("100k+")).toBe(true);
+  test("Check for invalid operator", () => {
+    expect(isEmployeeSize("10k-")).toBe(false);
+  });
+});
+
+// MARK: isCompanyName
+describe("Checking isCompanyName", () => {
+  test("Check for a valid name", () => {
+    expect(isCompanyName("e24")).toBe(true);
   });
 
-  test("testing for potential employee-size entry", () => {
-    expect(isEmployeeSize("100-100k")).toBe(false);
+  test("Check for a pure number", () => {
+    expect(isCompanyName("10")).toBe(false);
+  });
+
+  test("Check for incorrect employee size pattern", () => {
+    expect(isCompanyName("10-500")).toBe(false);
+  });
+
+  test("Check for a URL", () => {
+    expect(isCompanyName("https://facebook.com")).toBe(false);
+  });
+
+  test("Check for alphabetical name", () => {
+    expect(isCompanyName("Phleebs")).toBe(true);
   });
 });
